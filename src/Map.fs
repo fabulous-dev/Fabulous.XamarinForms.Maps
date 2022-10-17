c6ef22fc732e4944d42d5b5fb5d4e8be03e50a9f
Edgar <edgar.gonzalez@fundourselves.com>
2022-10-17 20:57:10 +0200

    Map support

--- src/Map.fs
+++ src/Map.fs
@@ -11,6 +11,38 @@ type IMap =
 
 module Map =
     let WidgetKey = Widgets.register<Map> ()
+    
+    let HasScrollEnabled = Attributes.defineBindableBool Map.HasScrollEnabledProperty
+    let HasZoomEnabled = Attributes.defineBindableBool Map.HasZoomEnabledProperty
+    let IsShowingUse = Attributes.defineBindableBool Map.IsShowingUserProperty
+    
+    let TrafficEnabledProperty = Attributes.defineBindableBool Map.TrafficEnabledProperty
+    
+    let MoveToLastRegionOnLayoutChange = Attributes.defineBindableBool Map.MoveToLastRegionOnLayoutChangeProperty
+    
+    let MapType =
+        Attributes.defineEnum<MapType>
+            "Map_MapType"
+            (fun _ newValueOpt node ->
+                let map = node.Target :?> Map
+
+                let value =
+                    match newValueOpt with
+                    | ValueNone -> MapType.Street
+                    | ValueSome v -> v
+
+                map.MapType <- value)
+            
+    let ItemsSource<'T> =
+        Attributes.defineBindable<WidgetItems<'T>, System.Collections.Generic.IEnumerable<Widget>>
+            Map.ItemsSourceProperty
+            (fun modelValue ->
+                seq {
+                    for x in modelValue.OriginalItems do
+                        modelValue.Template x
+                })
+            (fun a b -> ScalarAttributeComparers.equalityCompare a.OriginalItems b.OriginalItems)
+
 
 [<AutoOpen>]
 module MapBuilders =
---
+++ src/Pin.fs
@@ -0,0 +1,28 @@
+﻿namespace Fabulous.XamarinForms.Maps
+
+open System.Runtime.CompilerServices
+open Fabulous
+open Fabulous.StackAllocatedCollections.StackList
+open Fabulous.XamarinForms
+open Xamarin.Forms.Maps
+
+type IPin =
+    inherit Fabulous.XamarinForms.IElement
+
+module Pin =
+    let WidgetKey = Widgets.register<Pin> ()
+    
+[<AutoOpen>]
+module PinBuilders =
+    type Fabulous.XamarinForms.View with
+
+        /// Defines a Pin widget
+        static member inline Pin<'msg>() =
+            WidgetBuilder<'msg, IPin>(Pin.WidgetKey, AttributesBundle(StackList.empty (), ValueNone, ValueNone))
+
+[<Extension>]
+type PinModifiers =
+    /// <summary>Link a ViewRef to access the direct Pin control instance</summary>
+    [<Extension>]
+    static member inline reference(this: WidgetBuilder<'msg, IPin>, value: ViewRef<Pin>) =
+        this.AddScalar(ViewRefAttributes.ViewRef.WithValue(value.Unbox))
