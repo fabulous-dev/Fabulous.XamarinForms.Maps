be63939c9faf5d1f204d8a51475f1f7de6103457
Edgar <edgar.gonzalez@fundourselves.com>
2022-10-18 07:11:31 +0200

    Add a sample usage

--- samples/HelloMap/HelloMap/App.fs
+++ samples/HelloMap/HelloMap/App.fs
@@ -1,13 +1,49 @@
 namespace HelloMap
 
-open Xamarin.Forms
+open Fabulous
+open Xamarin.Forms.Maps
 open Fabulous.XamarinForms
 open Fabulous.XamarinForms.Maps
 
 open type Fabulous.XamarinForms.View
 
 module App =
-    let view () =
-        Application(ContentPage("HelloMap", Map()).ignoreSafeArea ())
 
-    let program = Program.stateless view
+    type Model = { Position: Position }
+
+    type Msg =
+        | MapClicked of Position
+        | MarkerClicked of bool
+
+    let init () =
+        { Position = Position(48.8566, 2.3522) }, Cmd.none
+
+    let update msg model =
+        match msg with
+        | MapClicked _ -> model, Cmd.none
+        | MarkerClicked _ -> model, Cmd.none
+
+    let private requestedRegion position =
+        MapSpan.FromCenterAndRadius(position, Distance.FromKilometers(25.))
+
+    let view (model: Model) =
+        Application(
+            ContentPage(
+                "HelloMap",
+                (Map(requestedRegion model.Position) {
+                    Pin(PinType.Place, "I'm a marker", model.Position)
+                        .address("My Address")
+                        .onMarkerClicked(MarkerClicked)
+                        .onInfoWindowClicked (MarkerClicked)
+                })
+                    .hasZoomEnabled(true)
+                    .hasScrollEnabled(true)
+                    .mapType(MapType.Street)
+                    .isShowingUser(true)
+                    .isTrafficEnabled(true)
+                    .onMapClicked (MapClicked)
+            )
+                .ignoreSafeArea ()
+        )
+
+    let program = Program.statefulWithCmd init update view
